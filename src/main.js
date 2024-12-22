64b9c81e753b871637a00e8e797aff0aaa8537ab
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-12-22 20:56:07 +0000

    Added function for opening recent projects.

--- src/main.js
+++ src/main.js
@@ -14,3 +14,20 @@ function openRecentBlogPost(recentBlogPostNumber){
         console.log("Please input a number from 1-3")
     }
 }
+
+// Function opens page for recent projects posts
+
+function openRecentProjectPost(recentProjectPostNumber){
+    if(recentProjectPostNumber === 1){
+        window.open()
+        console.log("Opened first recent project.")
+    } else if(recentProjectPostNumber === 2){
+        window.open()
+        console.log("Opened second recent project.")
+    } else if(recentProjectPostNumber === 3){
+        window,open()
+        console.log("Opened third recent project.")
+    } else{
+        console.log("Please input a number from 1-3")
+    }
+}
