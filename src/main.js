850eb07d0d4eee0ff9f52a7b8879b886d115f46c
Kieran Pritchard <KieranPritchard06@gmail.com>
2024-12-22 20:49:24 +0000

    Added function for opening recent blogs.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// Function opens page for recent blog posts
+
+function openRecentBlogPost(recentBlogPostNumber){
+    if(recentBlogPostNumber === 1){
+        window.open()
+        console.log("Opened first recent blog.")
+    } else if(recentBlogPostNumber === 2){
+        window.open()
+        console.log("Opened second recent blog.")
+    } else if(recentBlogPostNumber === 3){
+        window,open()
+        console.log("Opened third recent blog.")
+    } else{
+        console.log("Please input a number from 1-3")
+    }
+}
