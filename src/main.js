17652e41a8d452d91d463f65cbd3dbf2e93af79e
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-01-23 19:34:26 +0000

    Removed unused functions.

--- src/main.js
+++ src/main.js
@@ -1,36 +1,4 @@
-// Function opens page for recent blog posts
 
-function openRecentBlogPost(recentBlogPostNumber){
-    if(recentBlogPostNumber === 1){
-        window.open()
-        console.log("Opened first recent blog.")
-    } else if(recentBlogPostNumber === 2){
-        window.open()
-        console.log("Opened second recent blog.")
-    } else if(recentBlogPostNumber === 3){
-        window,open()
-        console.log("Opened third recent blog.")
-    } else{
-        console.log("Please input a number from 1-3")
-    }
-}
-
-// Function opens page for recent projects posts
-
-function openRecentProjectPost(recentProjectPostNumber){
-    if(recentProjectPostNumber === 1){
-        window.open()
-        console.log("Opened first recent project.")
-    } else if(recentProjectPostNumber === 2){
-        window.open()
-        console.log("Opened second recent project.")
-    } else if(recentProjectPostNumber === 3){
-        window,open()
-        console.log("Opened third recent project.")
-    } else{
-        console.log("Please input a number from 1-3")
-    }
-}
 
 // Function to search through blogs
 
