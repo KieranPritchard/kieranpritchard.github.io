aa80976b1d5e6201ef69a253392f4d2c9e218a4b
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-10-13 20:12:44 +0100

    added it new event listenr

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ window.addEventListener("DOMContentLoaded", function () {
     emailFormSize()
     initializeCarousels();
     setupTimelineObserver();
+    logScreenWidth();
 });
 
 // ============================================================================
